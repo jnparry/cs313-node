08a2dfa98e3495510f44f88313b846e19b15505f
Jordan <par16042@byui.edu>
2018-06-20 21:18:13 -0600

    Calc

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const PORT = (process.env.PORT || 5000);
+
+function calcRate(req, res) {
+    var requestUrl = url.parse(req.url, true);
+	console.log("Query parameters: " + JSON.stringify(requestUrl.query));
+    
+    res.render('pages/calc');
+}
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/calc', (req, res) => res.render('pages/calc'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    .use(express.static(path.join(__dirname, 'public')));
+    .set('views', path.join(__dirname, 'views'));
+    .set('view engine', 'ejs');
+    .get('/', (req, res) => res.render('pages/index'));
+    .get('/calc', function (req, res) {
+        calcRate(req, res);
+    })
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
