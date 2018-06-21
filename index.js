22a5cffe00b24cda9f1fb27a91bc858465a94d20
Jordan <par16042@byui.edu>
2018-06-20 21:29:35 -0600

    Calc

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ function calcRate(req, res) {
 }
 
 express()
-    .use(express.static(path.join(__dirname, 'public')));
-    .set('views', path.join(__dirname, 'views'));
-    .set('view engine', 'ejs');
-    .get('/', (req, res) => res.render('pages/index'));
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => res.render('pages/index'))
     .get('/calc', function (req, res) {
         calcRate(req, res);
     })
